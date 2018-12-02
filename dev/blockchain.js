2571a00ae7a1e4e9ee02a646939425a75e62e692
AghaAsadKhan <themragha@gmail.com>
2018-12-02 19:39:43 +0500

    sha256

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,3 +1,6 @@
+const sha256 = require('sha256');
+
+
 function Blockchain() {
     this.chain = [];
     this.penidingTransactions = [];
@@ -41,6 +44,15 @@ Blockchain.prototype.createNewTransaction = function(amount, sendr, recipient)
 
     return this.getLastBlock()['index'] + 1;
 }
+
+
+Blockchain.prototype.hashBlock = function(previousBlockHash, currentBlockData, nonce)
+{
+    const dataAsString = previousBlockHash + nonce.toString() + JSON.stringify(currentBlockData);
+    const hash = sha256(dataAsString);
+    return hash;
+}
+
 
 module.exports = Blockchain;
 
--- dev/test.js
+++ dev/test.js
@@ -3,7 +3,11 @@ const Blockchain = require('./blockchain');
 const bitcoin = new Blockchain();
 
 bitcoin.createNewBlock(2389, 'OKJSHSGAHHH00', 'KAJAHSGBD790KJ');
-
 bitcoin.createNewTransaction(100, 'ALXjjsjjssjsj', 'JENNoojjshshshshkj');
+bitcoin.createNewBlock(2000, 'OKJSHSGAHHH00', 'kjhagghhdd79');
 
-console.log(bitcoin);
+bitcoin.createNewTransaction(50, 'A2Xjjsjjssjsj', 'A2Xjjsjjssjsj');
+bitcoin.createNewTransaction(40, 'A3Xjjsjjssjsj', 'A3Xjjsjjssjsj');
+
+bitcoin.createNewBlock(1000, 'OKppHSGAHHH00', 'kojhagghhdd79');
+console.log(bitcoin.chain[2]);
